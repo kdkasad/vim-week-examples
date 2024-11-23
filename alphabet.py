76bd20dc45705baffd3d69a100c2df6c4df657c6
Kian Kasad <kian@kasad.com>
2024-11-23 16:11:58 -0500

    Change alphabet.py to Rust

--- alphabet.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-alphabet = <++>
-
-print(reversed_alphabet)
---
+++ alphabet.rs
@@ -0,0 +1,7 @@
+const alphabet: &'static str = "";
+const reversed_alphabet: &'static str = "";
+
+fn main() {
+    println!("Alphabet: {}", alphabet);
+    println!("Alphabet (reversed): {}", reversed_alphabet);
+}
